9968bc6f8dbe6ab0ee44b0787a8c7e6b05593e4a
Ofonna-N <nwezeofonna@gmail.com>
2023-06-19 12:15:28 -0500

    Added css minimizer plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
@@ -13,6 +14,13 @@ module.exports = merge(common, {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      `...`,
+      new CssMinimizerPlugin(),
     ],
   },
 });
